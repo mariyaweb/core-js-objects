4eabdd82e762b52f37df390107864ceebcb27708
github-mariyaweb <96mariyask@gmail.com>
2024-03-08 17:38:02 +0300

    feat: add 3 task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -60,8 +60,16 @@ function mergeObjects(objects) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, 'age') => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  const copyObj = shallowCopy(obj);
+  Object.entries(obj).forEach(([objKey]) => {
+    keys.forEach((key) => {
+      if (key === objKey) {
+        delete copyObj[key];
+      }
+    });
+  });
+  return copyObj;
 }
 
 /**
